626c3e18690f6ab642e585474a88ee2e9d0f63ac
bySharkster <fernandoaponte0609@gmail.com>
2021-02-22 15:01:52 -0400

    yourwallet 1st commit

---
+++ frontend/build/precache-manifest.8a2c10404a6dde1aeac4c017315f2d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50a9ee748084d1973524",
+    "url": "/static/css/main.bddee462.chunk.css"
+  },
+  {
+    "revision": "50a9ee748084d1973524",
+    "url": "/static/js/main.08d6df39.chunk.js"
+  },
+  {
+    "revision": "edc59d3f24078970bce9",
+    "url": "/static/js/runtime~main.12dfdcff.js"
+  },
+  {
+    "revision": "f96f2dd26574e1b69686",
+    "url": "/static/js/2.beaedf1f.chunk.js"
+  },
+  {
+    "revision": "e25697acbd1517f38dc3",
+    "url": "/static/js/3.ca20fdc0.chunk.js"
+  },
+  {
+    "revision": "6006f1a0760aeb7c3f2e27d0522ed1e2",
+    "url": "/index.html"
+  }
+];
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {LinkContainer} from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+const Header = () => {
+    return (
+    <header>
+<Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+    <Container>
+      <LinkContainer to='/'>
+        <Navbar.Brand >YourWallet</Navbar.Brand>
+      </LinkContainer>
+  
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <LinkContainer to='/cart'>
+          <Nav.Link ><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/login'>
+          <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+        </LinkContainer>
+
+
+        <LinkContainer to='/seeFinance'>
+          <Nav.Link><i className='fas fa-wallet'></i>Finance</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/addFinance'>
+          <Nav.Link><i className='fas fa-money-bill-wave-alt'></i>Add Finance</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/aboutUs'>
+          <Nav.Link><i className='fas fa-user-astronaut'></i>About Us</Nav.Link>
+        </LinkContainer>
+
+        
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    </header>)
+}
+
+export default Header
