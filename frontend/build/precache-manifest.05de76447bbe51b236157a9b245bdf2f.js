902e2ff181e498b1fcc19a6623a4421f1af24c95
bySharkster <fernandoaponte0609@gmail.com>
2021-03-02 23:38:25 -0400

    gitignore

---
+++ frontend/build/precache-manifest.05de76447bbe51b236157a9b245bdf2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "473b860b4ecfcc1a1569",
+    "url": "/static/css/main.35ea838c.chunk.css"
+  },
+  {
+    "revision": "473b860b4ecfcc1a1569",
+    "url": "/static/js/main.b51d2185.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75147d7d2d29cec9d166",
+    "url": "/static/js/2.972bdae1.chunk.js"
+  },
+  {
+    "revision": "047fdcf909c42aba624ba7fdedfdc342",
+    "url": "/index.html"
+  }
+];
